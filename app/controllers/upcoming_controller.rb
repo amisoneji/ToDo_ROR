70f2d1db44fef76a52b81ab6384e70e8da3e69a2
amisoneji <amisoneji92@gmail.com>
2022-03-23 10:15:21 +0530

    pg added

--- app/controllers/upcoming_controller.rb
+++ app/controllers/upcoming_controller.rb
@@ -1,7 +1,7 @@
 class UpcomingController < ApplicationController
 	def upcomingtask
-		@todo_lists = Current.user.todo_lists
-		@upcomings=todo_lists.where('@todo_lists.Date > ?', 1.days.from_now)
+		todo_lists = Current.user.todo_lists
+		@upcomings=todo_lists.where('todo_lists.Date > ?', 1.days.from_now)
 	end
 
 end
